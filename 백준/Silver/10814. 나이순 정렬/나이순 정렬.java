12cca52520521cf77c53b98fba7b1be456ced65f
Hmm <78787149+wnd06@users.noreply.github.com>
2023-05-15 18:41:30 +0900

    [Silver V] Title: 나이순 정렬, Time: 1624 ms, Memory: 55596 KB -BaekjoonHub

---
+++ 백준/Silver/10814. 나이순 정렬/나이순 정렬.java
@@ -0,0 +1,38 @@
+
+import java.io.*;
+import java.nio.Buffer;
+import java.util.*;
+
+/**
+ * Scanner로 입력받지 않고,
+ * BufferedReader, InputStreamReader, StringTokenizer
+ * 를 통해 입력 받는 것 공부해오기
+ */
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
+        int N = Integer.parseInt(br.readLine());
+        String [][] arr = new String [N][2];
+
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            arr[i][0] = st.nextToken();
+            arr[i][1] = st.nextToken();
+        }
+        Arrays.sort(arr,(e1, e2) -> {
+            if(Integer.parseInt(e1[0]) - Integer.parseInt(e2[0]) < 0)
+                return Integer.parseInt(e1[0]) - Integer.parseInt(e2[0]);
+            else
+                return Integer.parseInt(e1[0]) - Integer.parseInt(e2[0]);
+        });
+
+        for (int i = 0; i < N; i++) {
+            System.out.println(arr[i][0] + " " + arr[i][1]);
+        }
+
+    }
+
+}
