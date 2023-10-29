c6efc4d99485ec134cef99c8c4d56b0469cf1e39
Hmm <78787149+wnd06@users.noreply.github.com>
2023-10-29 15:13:02 +0900

    [D3] Title: [S/W 문제해결 기본] 1일차 - Flatten, Time: 121 ms, Memory: 21,816 KB -BaekjoonHub

--- SWEA/D3/1208. ［S／W 문제해결 기본］ 1일차 － Flatten/［S／W 문제해결 기본］ 1일차 － Flatten.java
+++ SWEA/D3/1208. ［S／W 문제해결 기본］ 1일차 － Flatten/［S／W 문제해결 기본］ 1일차 － Flatten.java
@@ -2,42 +2,33 @@ import java.io.*;
 import java.util.*;
 
 public class Solution {
-    static int max;
-    static int min;
+    static int[] width;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int T = 10;
         for (int i = 1; i <= T; i++) {
-            max = 0;
-            min = 0;
             int dumpChance = Integer.parseInt(br.readLine());
-            int[] width = new int[100];
+            width = new int[100];
             StringTokenizer st = new StringTokenizer(br.readLine());
             for (int j = 0; j < width.length; j++) {
                 width[j] = Integer.parseInt(st.nextToken());
             }
-            int result = search(dumpChance, width);
-            System.out.println("#" + i + " " + result);
+            search(dumpChance, width);
+            Arrays.sort(width);
+            System.out.println("#" + i + " " + (width[width.length-1] - width[0]));
         }
     }
-    public static int search(int dumpChance, int[] width) {
-        int answer = result(width);
+    public static void search(int dumpChance, int[] width) {
 
-        if(dumpChance == 0) return answer;
-        else return search(dumpChance - 1, width);
+        if(dumpChance == 0) return;
 
-    }
-
-    public static int result(int[] width) {
         Arrays.sort(width);
+        width[0]++;
+        width[width.length-1]--;
 
-        min = width[0];
-        max = width[width.length - 1];
+        search(dumpChance - 1, width);
 
-        width[0] += 1;
-        width[width.length - 1] -= 1;
-
-        return max-min;
     }
+
 }
