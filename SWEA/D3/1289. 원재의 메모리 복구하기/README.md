367be00ec896275c3e9e16e76f8ab2b0d31a03e8
Hmm <78787149+wnd06@users.noreply.github.com>
2023-11-05 21:13:21 +0900

    [D3] Title: 원재의 메모리 복구하기, Time: 137 ms, Memory: 20,356 KB -BaekjoonHub

---
+++ SWEA/D3/1289. 원재의 메모리 복구하기/README.md
@@ -0,0 +1,15 @@
+# [D3] 원재의 메모리 복구하기 - 1289 
+
+[문제 링크](https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AV19AcoKI9sCFAZN) 
+
+### 성능 요약
+
+메모리: 20,356 KB, 시간: 137 ms, 코드길이: 1,082 Bytes
+
+### 제출 일자
+
+2023-11-05 21:13
+
+
+
+> 출처: SW Expert Academy, https://swexpertacademy.com/main/code/problem/problemList.do
---
+++ SWEA/D3/1289. 원재의 메모리 복구하기/원재의 메모리 복구하기.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+import java.io.FileInputStream;
+
+/*
+   사용하는 클래스명이 Solution 이어야 하므로, 가급적 Solution.java 를 사용할 것을 권장합니다.
+   이러한 상황에서도 동일하게 java Solution 명령으로 프로그램을 수행해볼 수 있습니다.
+ */
+class Solution
+{
+    public static void main(String args[]) throws Exception
+    {
+        //System.setIn(new FileInputStream("res/input.txt"));
+
+		/*
+		   표준입력 System.in 으로부터 스캐너를 만들어 데이터를 읽어옵니다.
+		 */
+        Scanner sc = new Scanner(System.in);
+        int T;
+        T=sc.nextInt();
+		/*
+		   여러 개의 테스트 케이스가 주어지므로, 각각을 처리합니다.
+		*/
+
+        for(int test_case = 1; test_case <= T; test_case++)
+        {
+            String bit = sc.next();
+            int count = 0;
+            char num = '0';
+            for (int i = 0; i < bit.length(); i++) {
+                if(bit.charAt(i) != num) {
+                    num = bit.charAt(i);
+                    count++;
+                }
+            }
+            
+            System.out.println("#" +test_case + " " + count);
+        }
+    }
+}
